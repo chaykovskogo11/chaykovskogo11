41a5a0b45e5f5c72e46191662061cc3da34fbe6a
George <penkov.gd@gmail.com>
2022-10-20 13:26:59 +0300

    рубрики на index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let nav_button = document.querySelector(".nav-button");
-let list_of_contents = document.querySelector(".list-of-contents");
-let list_of_articles = document.querySelector(".list-of-articles");
+const nav_button = document.querySelector(".nav-button");
+const list_of_contents = document.querySelector(".list-of-contents");
+const list_of_articles = document.querySelector(".list-of-articles");
 
 nav_button.addEventListener('click', function () {
     if (list_of_articles.style.display === 'none') {
@@ -10,4 +10,10 @@ nav_button.addEventListener('click', function () {
         list_of_contents.style.display = 'none'
         list_of_articles.style.display = 'none'
     }
+});
+
+const card = document.querySelector(".card");
+const main_link = document.querySelector(".main-link")
+card.addEventListener("click", function () {
+    main_link.click();
 });
